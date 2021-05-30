8b6c1800c02ab2dd462f56ac9c670101f50c8c2e
Adel NOUAR <adel.nouar@protonmail.com>
2021-05-30 15:44:55 +0200

    Some code to test and learn

--- src/DocBlog/views.py
+++ src/DocBlog/views.py
@@ -1,6 +1,8 @@
+from datetime import datetime
 from django.shortcuts import render
 
 
 def index(request):
     # return HttpResponse("<h1> Hello, welcome to my website </h1>")
-    return render(request, "index.html")
+    date = datetime.today()
+    return render(request, "index.html", context={'prenom': 'ana', 'date': date})
