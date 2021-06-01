bc61e829accc2fb47372dd9c1f3970c237350780
Adel NOUAR <adel.nouar@protonmail.com>
2021-06-01 20:29:02 +0200

    Some testing on Django

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index, article_01, article_02, article_03
 
 urlpatterns = [
-    path('', index, name='blog-index')
+    path('', index, name='blog-index'),
+    path('article_01/', article_01, name='blog-article-01'),
+    path('article_02/', article_02, name='blog-article-02'),
+    path('article_03/', article_03, name='blog-article-03')
 ]
--- src/blog/views.py
+++ src/blog/views.py
@@ -5,3 +5,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'blog/index.html')
+
+
+def article_01(request):
+    return render(request, 'blog/article_01.html')
+
+
+def article_02(request):
+    return render(request, 'blog/article_02.html')
+
+
+def article_03(request):
+    return render(request, 'blog/article_03.html')
